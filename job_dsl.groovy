56b23e16e1c6f80e5b122b8721d31b1e9bddb87d
Antoine <me@antoinebellanger.fr>
2025-03-14 16:46:28 +0100

    feat: clone-repository job

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,9 +1,16 @@
 folder('Tools') {
     description('Folder for miscellaneous tools.')
+}
 
-    job('clone-repository') {
-        configure { node ->
-            node << 'test'
-        }
+freeStyleJob('Tools/clone-repository') {
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', null, 'Git URL of the repository to clone')
+    }
+    steps {
+        shell('git clone ${GIT_REPOSITORY_URL}')
+    }
+    wrappers {
+        preBuildCleanup()
     }
 }
+
