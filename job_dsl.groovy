869df9cc2d166267f5b42c7d414673adf2df9d84
Antoine <me@antoinebellanger.fr>
2025-03-18 13:57:04 +0100

    fix: seed job and it's childs

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,10 +1,35 @@
+def seeded_jobs_dsl = 
+"""freeStyleJob(DISPLAY_NAME) {
+    properties {
+        githubProjectProperty {
+            projectUrlStr(GITHUB_NAME)
+            displayName(DISPLAY_NAME)
+        }
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    scm {
+        github(GITHUB_NAME)
+    }
+    steps {
+        shell('make fclean')
+        shell('make')
+        shell('make tests_run')
+        shell('make clean')
+    }
+    triggers {
+        scm('* * * * *')
+    }
+}"""
+
 folder('Tools') {
     description('Folder for miscellaneous tools.')
 }
 
 freeStyleJob('Tools/clone-repository') {
     parameters {
-        stringParam('GIT_REPOSITORY_URL', null, 'Git URL of the repository to clone')
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
     }
     steps {
         shell('git clone ${GIT_REPOSITORY_URL}')
@@ -14,3 +39,14 @@ freeStyleJob('Tools/clone-repository') {
     }
 }
 
+freeStyleJob('Tools/SEED') {
+    parameters {
+        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+        stringParam('DISPLAY_NAME', '', 'Display name of the job')
+    }
+    steps {
+        systemGroovyCommand(dsl(seeded_jobs_dsl)) {
+            sandbox(true)
+        }
+    }
+}
